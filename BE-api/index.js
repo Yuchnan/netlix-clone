bec176b752a16af1a0bf1ad19a8c71da6515b609
Yuchnan <ramandhanuyuchnan@gmail.com>
2024-12-05 19:33:18 +0700

    dokumentasi API menggunakan swagger-ui

--- BE-api/index.js
+++ BE-api/index.js
@@ -4,6 +4,9 @@ const express = require("express")
 const cors = require("cors")
 const routes = require("./routes/index.route")
 const mongoose = require("mongoose")
+const swaggerUI = require("swagger-ui-express")
+const YAML = require("yamljs")
+const swaggerDocs = YAML.load("./swagger.yaml")
 
 const { API_PORT, MONGO_URL } = process.env
 
@@ -12,6 +15,14 @@ const PORT = API_PORT
 
 app.use(express.json())
 app.use(cors()) 
+
+app.use(
+    "/docs",
+    swaggerUI.serve,
+    swaggerUI.setup(swaggerDocs)
+)
+
+// http://localhost:3002/docs
 
 mongoose.connect(MONGO_URL)
     .catch(err => {
--- BE-api/tests/index.test.js
+++ BE-api/tests/index.test.js
@@ -12,6 +12,45 @@ afterEach(async () => {
 })
 
 describe('Resource /my-movies', () => {
+    //sign in token
+    it('Should return user not found', async () => {
+        const response = await request(app)
+            .post("/my-token")
+            .set("Content-Type", "application/json")
+            .send({
+                email: "yuchnan123@gmail.com",
+                password: "qwerty",
+                token: "6969"
+            })
+        expect(response.statusCode).toBe(400)
+        expect(response.body.message).toBe("User Not Found!")
+    })
+
+    it('Should return wrong password', async () => {
+        const response = await request(app)
+            .post("/my-token")
+            .set("Content-Type", "application/json")
+            .send({
+                email: "yuchnan@gmail.com",
+                password: "qwerty123",
+            })
+        expect(response.statusCode).toBe(400)
+        expect(response.body.message).toBe("Wrong Password!")
+    })
+
+    it('Should return sign in token success', async () => {
+        const response = await request(app)
+            .post("/my-token")
+            .set("Content-Type", "application/json")
+            .send({
+                email: "yuchnan@gmail.com",
+                password: "qwerty",
+                token: "6969"
+            })
+        expect(response.statusCode).toBe(200)
+        expect(response.body.message).toBe("Sign-in Token Saved!")
+    })
+
     //get fav movies
     it('Should return success message', async () => {
         const response = await request(app).get(
@@ -91,45 +130,6 @@ describe('Resource /my-movies', () => {
         expect(response.body.message).toBe("Remove Favorite Movies Success!")
     })
 
-    //sign in token
-    it('Should return user not found', async () => {
-        const response = await request(app)
-            .post("/my-token")
-            .set("Content-Type", "application/json")
-            .send({
-                email: "yuchnan123@gmail.com",
-                password: "qwerty",
-                token: "6969"
-            })
-        expect(response.statusCode).toBe(400)
-        expect(response.body.message).toBe("User Not Found!")
-    })
-
-    it('Should return wrong password', async () => {
-        const response = await request(app)
-            .post("/my-token")
-            .set("Content-Type", "application/json")
-            .send({
-                email: "yuchnan@gmail.com",
-                password: "qwerty123",
-            })
-        expect(response.statusCode).toBe(400)
-        expect(response.body.message).toBe("Wrong Password!")
-    })
-
-    it('Should return sign in token success', async () => {
-        const response = await request(app)
-            .post("/my-token")
-            .set("Content-Type", "application/json")
-            .send({
-                email: "yuchnan@gmail.com",
-                password: "qwerty",
-                token: "6969"
-            })
-        expect(response.statusCode).toBe(200)
-        expect(response.body.message).toBe("Sign-in Token Saved!")
-    })
-
     //sign-out token
     it('Should return sign out success', async () => {
         const response = await request(app)
@@ -156,14 +156,16 @@ describe('Resource /my-movies', () => {
         expect(response.body.message).toBe("Email Not Available!")
     })
 
+    let count = 0;
     it('Should return sign up success', async () => {
         const response = await request(app)
             .post("/sign-up")
             .set("Content-Type", "application/json")
             .send({
-                email: "ramandhanu123@gmail.com",
-                password: "2222"
+                email: "ramandhanu" + (count + 1) + "@gmail.com",
+                password: "123"
             })
+        count++;
         expect(response.statusCode).toBe(201)
         expect(response.body.message).toBe("Sign-up Success!")
     })
