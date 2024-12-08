cbaf8b3be94e7adc30c94e8fe2f5541ed27dcc0d
Yuchnan <ramandhanuyuchnan@gmail.com>
2024-12-08 12:40:16 +0700

    menambahkan atom isFavorited

--- FE-react/src/jotai/atoms.js
+++ FE-react/src/jotai/atoms.js
@@ -5,6 +5,7 @@ export const languageAtom = atomWithStorage("language", "id")
 export const tokenAtom = atomWithStorage("token", null)
 export const emailStorageAtom = atomWithStorage("email", null)
 
+export const isFavoritedAtom = atom(false)
 export const idMovieAtom = atom(null)
 export const isOpenModalAtom = atom(false)
 export const searchMoviesAtom = atom(null)
